5c3542d675fcd82025bcabc85f68f3ecca2cbadf
Rachid <rachid@MBP-de-Rachid.lan>
2025-10-20 11:26:36 +0200

    Appeler l'api de l'exterieur tout le temps

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: '/api',
+  // Ancien accès local via proxy: '/api' -> http://192.168.1.92:8080/api
+  apiUrl: 'http://176.177.13.58:1982/api',
   basicAuth: {
     username: 'rachid',
     password: 'rachid123'
